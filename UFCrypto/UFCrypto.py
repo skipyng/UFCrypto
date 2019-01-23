c3ed710a68cc1d107885cd97899d521fed6c9862
michele.schelfi <michele.schelfi@TNS2-5>
2019-01-23 10:59:29 +0100

    robe

--- UFCrypto/UFCrypto.py
+++ UFCrypto/UFCrypto.py
@@ -1,1 +1,24 @@
+import json
+from base64 import b64encode
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
+class Crittografia:
+    def __init__(self,keyBytes:int,authentication:bool):
+        self.__key:int = generateKey(keyBytes)
+        self.__auth:bool = authentication
+
+    @property
+    def key(self):
+        return self.__key
+
+    @property
+    def cipher(self):
+        return self.__cipher
+     
+    def generateKey(bytes):
+        return get_random_bytes(bytes)
+
+    def generateCipher():
+        if self.__auth:
 
