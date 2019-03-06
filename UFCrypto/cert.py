1cd7d5a84d0cc368184054002b3208648b0ed5b3
Alta Formazione <afp@Mob2-26.Marconi.loc>
2019-03-06 15:53:04 +0000

    Aggiungi generazione certificato

--- UFCrypto/cert.py
+++ UFCrypto/cert.py
@@ -2,85 +2,35 @@
 import json, os, sys
 import traceback
 from base64 import b64encode, b64decode
-from Crypto.PublicKey import RSA
+from Crypto.PublicKey import RSA, ECC
 from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.Protocol import KDF
 from Crypto.Random import get_random_bytes
 from getpass import getpass
 
-  
-class Crittografia():
-    def __init__(self):
-        self.__pubkey = None
-        self.__passw = None
-        self.__salt = None
-        self.__crypted = None
-    
-    def GenerateKey(self, password:str):
-        try:            
-            key = RSA.generate(2048)
-            self.__privkey = key.export_key(passphrase = password)
-            self.__pubkey = key.publickey()
-           
-        except Exception as e:
-            print(str(e))
-            input()
-         
-    def ImportPubKey(self,key:bytes):
-        self.__pubkey = RSA.import_key(key)
-
-    def ImportPrivKey(self,key:bytes, password:str):
-        self.__privkey = RSA.import_key(key,password)
-
-    def Crypt(self,content:bytes):
-        cipher = PKCS1_OAEP.new(self.__pubkey)
-        sessionKey = get_random_bytes(16)
-        encSessionKey = cipher.encrypt(sessionKey)
-
-        ciphAes = AES.new(sessionKey,AES.MODE_EAX)
-        self.__crypted, tag = ciphAes.encrypt_and_digest(content)
-
-        self.__jsonKeys = ['nonce', 'cyphertext', 'tag', 'enckey']
-        self.__jsonVal = [ciphAes.nonce, self.__crypted, tag, encSessionKey]
-
-    def Decrypt(self,dictIn:dict):
-        cipher = PKCS1_OAEP.new(self.__privkey)
-        sessionkey = cipher.decrypt(b64decode(dictIn['enckey']))
-
-        ciphAes = AES.new(sessionkey,AES.MODE_EAX, nonce = b64decode(dictIn['nonce']))
-        return ciphAes.decrypt_and_verify(b64decode(dictIn['cyphertext']), b64decode(dictIn['tag']))
-
-    def serialize(self):
-        json_values = [b64encode(x).decode('utf-8') for x in self.__jsonVal]
-        return bytes(json.dumps(dict(zip(self.__jsonKeys, json_values))),'utf-8')
-
-    def deserialize(self, input:str):
-        return json.loads(input)
-
-    @property
-    def resJSON(self):
-        return self.serialize()
-
-    @property
-    def PubKey(self):
-        return self.__pubkey
-
-    @property
-    def PrivKey(self):
-        return self.__privkey
-
-
 class Certificato():
     def __init__(self):
-        self.__pubkey = None
-        self.__cert = None
-        self.__resJSON = None
+        self.__pubCA = """-----BEGIN PUBLIC KEY-----
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEdqBTMZ+Mmv9mYlvXE410J8rpWfm/
+vxl6y+pWhVFLPKNs++iyWCiuTP+Y3un7c4ACzfwn++aDG/Gf4yWI0S0WPg==
+-----END PUBLIC KEY-----"""
     
-    def GeneraCert(self, crit:Crittografia, id:str, password:str):
-        crit.GenerateKey(password)
-        self.__pubkey = crit.PubKey
-        keys = ['id','pubk','sig']
-        self.__resJSON = self.Serialize(keys,[bytes(id,'utf-8'),self.__pubkey.export_key(),bytes('','utf-8')])
+    def GeneraCert(self, id:str, password:str):
+        self.__key = ECC.generate(curve='P-256')
+        prk_settings = {
+            'format': 'PEM',
+            'passphrase': password,
+            'protection': 'scryptAndAES256-CBC'
+            }
+        self.__privkey = self.__key.export_key(**prk_settings)
+        self.__pubkey = self.__key.public_key()
+        self.Serialize(id)
+
+    def ImportKey(self, keyIn:str, type:str, psw=None):
+        if type == "pub":
+            self.__pubkey = ECC.import_key(keyIn)
+        elif type == "priv":
+            self.__privkey = ECC.import_key(keyIn,psw)
 
     def VerificaFirma(self):
         pass
@@ -88,14 +38,22 @@ class Certificato():
     def Firma(self):
         pass
     
-    def Serialize(self, keys:list, values:list):
-        tmp = [b64encode(x).decode('utf-8') for x in values]
-        return bytes(json.dumps(dict(zip(keys, tmp))),'utf-8')    
+    def Serialize(self, id:str):
+        pubk_str = self.__pubkey.export_key(format='PEM')
+        print(pubk_str)
+        tmp = {'id': id, 'pubk':pubk_str,'sig':''}
+        self.__resJSON = json.dumps(tmp).encode('utf-8')
+
+    def Deserialize(self,content:str):
+        return json.loads(content)
 
     @property
     def resJSON(self):
         return self.__resJSON
 
+    @property
+    def Privkey(self):
+        return self.__privkey
 # Definizione funzione "clear terminal" #
 def clear():
     if os.name == 'nt':
@@ -135,50 +93,32 @@ def showPrompt(type:str):
 ##############################################
 
 while True:
-    obj = Crittografia()
     cert = Certificato()
     clear()
     tmp = showPrompt("init")
     if tmp == 1:
         try:
-            # Scelta utilizzo chiavi #
+            # Scelta utilizzo certificato #
             importPub = input("Generare un certificato? S/N ")
             if importPub.upper() == "S": 
                 psw = showPrompt("password")
-                cert.GeneraCert(obj,'micheleschelfi',psw)                
-                print("CERTIFICATO GENERATO")
+                cert.GeneraCert('micheleschelfi',psw)                
+                print("GENERAZIONE CERTIFICATO...")
                 clear()
-                print("JSON CERTIFICATO")
+                print("CERTIFICATO GENERATO")
                 path = showPrompt("path")
                 print("\nFile salvato in: ["+saveFile(path+".cert",cert.resJSON)+"]")
-
+                print("\nChiave privata salvata in: ["+saveFile("key.priv",cert.Privkey.encode('utf-8'))+"]")
             elif importPub.upper() == "N": 
                 clear()
-                print("GENERAZIONE COPPIA DI CHIAVI\n")
-                print("\n\t------------ PASSWORD ------------ ")
-                psw = showPrompt("password")
-                clear()
-                print("Generazione chiavi in corso...")
-                obj.GenerateKey(psw)
-                clear()
-                print("\n CHIAVE GENERATA")
+                print("CERTIFICATO ESISTENTE")
                 path = showPrompt("path")
-                # Salvataggio chiavi pubbliche e private #
-                print("\nChiave pubblica salvata in: ["+saveFile(path+".pub",obj.PubKey.export_key())+"]")
-                print("\nChiave privata salvata in: ["+saveFile(path+".priv",obj.PrivKey)+"]")
-                input("\nPremi INVIO per continuare")
                 clear()
             else:
                 print("Parametro non valido")
                 continue
             # File "bersaglio" #
-            print("FILE DA CRIPTARE")
-            path = showPrompt("path")
-            print("Crittazione in corso...")
-            obj.Crypt(readFile(path))
-            print("File criptato.")
-            print("\nFile salvato in: ["+saveFile(path+".crypt",obj.resJSON)+"]")
-            input("\nPremi INVIO per continuare")
+           
         except Exception as e:
             tb = traceback.format_exc()
             print(tb) # SOLO PER DEBUG
@@ -189,15 +129,8 @@ while True:
             print("\nCHIAVE PRIVATA")
             path = showPrompt("path")
             psw = getpass("Inserisci la password per la chiave privata: ")
-            obj.ImportPrivKey(readFile(path),psw)
-            print("\n FILE CRIPTATO")
-            path = showPrompt("path")
-            tmp = obj.Decrypt(json.loads(readFile(path)))
-            clear()
-            print("FILE DECRITTATO")
-            path = showPrompt("path")
-            print("\nFile decrittato salvato in: ["+saveFile(path,tmp)+"]")
-            input("\nPremi INVIO per continuare")
+            cert.ImportKey(readFile(path),'priv',psw)
+            
         except Exception as e:
             tb = traceback.format_exc()
             if str(e) == "MAC check failed":
